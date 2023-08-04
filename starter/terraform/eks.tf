afe03a8c89bf5befa975ddcc0f709644fe1ce873
Nestor Saavedra <31100486+TheAstronauta@users.noreply.github.com>
2023-08-03 22:02:58 -0700

    up

--- starter/terraform/eks.tf
+++ starter/terraform/eks.tf
@@ -1,5 +1,6 @@
 provider "kubernetes" {
-   config_path            = "~/.kube/config"
+   //config_path            = "~/.kube/config"
+   config_path            = "/Users/nestorsaavedra/.kube/config"
    host                   = data.aws_eks_cluster.cluster.endpoint
    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
    token                  = data.aws_eks_cluster_auth.cluster.token
